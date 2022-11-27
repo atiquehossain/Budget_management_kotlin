4f0a8551b43305f2560ce522e1b2b5d86b767560
atique.hossain <atique.hossain@savethechildren.org>
2022-11-27 16:04:34 +0600

    homeUI

--- app/src/main/java/com/example/budgetmanagement/HomeActivity.kt
+++ app/src/main/java/com/example/budgetmanagement/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.example.budgetmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottomNav = findViewById(R.id.bottomNavigationView) as BottomNavigationView
+
+        bottomNav.setOnNavigationItemReselectedListener {
+
+        }
     }
 }
