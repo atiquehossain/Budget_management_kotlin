fd6013d1da31b08f5799a39f2f17a53aee6716da
atique.hossain <atique.hossain@savethechildren.org>
2022-11-28 11:39:40 +0600

    Fragment_implement

--- app/src/main/java/com/example/budgetmanagement/HomeActivity.kt
+++ app/src/main/java/com/example/budgetmanagement/HomeActivity.kt
@@ -2,17 +2,44 @@ package com.example.budgetmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.budgetmanagement.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var bottomNav : BottomNavigationView
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        replaceFragment(HomeFragment())
+
+
         bottomNav = findViewById(R.id.bottomNavigationView) as BottomNavigationView
 
-        bottomNav.setOnNavigationItemReselectedListener {
+
+        bottomNav.setOnItemSelectedListener {
+
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.history -> replaceFragment(HistoryFragment())
+
+                else ->{
+
+                }
+            }
+            true
 
         }
+
+    }
+
+    private  fun replaceFragment(fragment: Fragment){
+        val  fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
